2bf15887e01b885d1d0cc9a263b35d16e7a39407
Phil Sturgeon <email@philsturgeon.co.uk>
2014-03-19 01:02:51 -0400

    Starting magic forms.

--- system/cms/themes/pyrocms/views/admin/login.php
+++ system/cms/themes/pyrocms/views/admin/login.php
@@ -4,15 +4,15 @@
 	<meta charset="utf-8">
 	<meta name=viewport content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
 	<meta name="robots" content="noindex,nofollow">
-	<title><?php echo $this->settings->site_name; ?> - <?php echo lang('login_title');?></title>
+	<title><?= $this->settings->site_name; ?> - <?= lang('login_title');?></title>
 
-	<base href="<?php echo base_url(); ?>"/>
+	<base href="<?= base_url(); ?>"/>
 	<meta name="robots" content="noindex, nofollow"/>
 
 	<?php Asset::css(array('build.css')); ?>
 	<?php Asset::js(array('jquery.min.js', 'modernizr.min.js')); ?>
 
-	<?php echo Asset::render() ?>
+	<?= Asset::render() ?>
 </head>
 
 <body id="login">
@@ -22,41 +22,40 @@
 	<div class="row-fluid">
 	<div class="col-md-3 col-md-offset-4 p">
 
-		
 		<center id="page-title" class="brand">
-			<h2><?php echo Asset::img('icon-logo-darker.png', 'PyroCMS', array('height' => '40px')); ?> <strong>Pyro</strong>CMS</h2>
+			<h2><?= Asset::img('icon-logo-darker.png', 'PyroCMS', ['height' => '40px']); ?> <strong>Pyro</strong>CMS</h2>
 		</center>
 
 		<?php $this->load->view('admin/partials/notices') ?>
 
 		<section>
 
-			<?php echo form_open('admin/login', array('id' => 'login', 'class' => 'form margin-top')); ?>
+			<?= Form::open(['url' => 'admin/login', 'id' => 'login', 'class' => 'form margin-top']) ?>
 
 			<div class="form-group">
 				<label for="remember-check">Email</label>
-				<input type="text" name="email" class="form-control" placeholder="<?php echo lang('global:email'); ?>"/>
+				<input type="text" name="email" class="form-control" placeholder="<?= lang('global:email'); ?>"/>
 			</div>
 
 			<div class="form-group">
 				<label for="remember-check">Password</label>
-				<input type="password" name="password" class="form-control" placeholder="<?php echo lang('global:password'); ?>"/>
+				<input type="password" name="password" class="form-control" placeholder="<?= lang('global:password'); ?>"/>
 			</div>
 
 			<div class="form-group">
 				<label for="remember-check">
 					<input type="checkbox" name="remember" id="remember-check" checked />
-					<?php echo lang('user:remember'); ?>
+					<?= lang('user:remember'); ?>
 				</label>
 
 				<br/>
 			
-				<button class="btn btn-primary btn-block" type="submit" name="submit" value="<?php echo lang('login_label'); ?>">
-					<span><?php echo lang('login_label'); ?></span>
+				<button class="btn btn-primary btn-block" type="submit" name="submit" value="<?= lang('login_label'); ?>">
+					<span><?= lang('login_label'); ?></span>
 				</button>
 			</div>
 
-			<?php echo form_close(); ?>
+			<?= Form::close(); ?>
 
 		</section>
 
@@ -70,8 +69,8 @@
 	<center class="container">
 		<div class="row-fluid">
 		<div class="col-md-3 col-md-offset-4 p animated-fast fadeInUp">
-			Copyright &copy; 2009 - <?php echo date('Y'); ?> PyroCMS LLC 
-			<br><span id="version"><?php echo CMS_VERSION.' '.CMS_EDITION; ?></span>
+			Copyright &copy; 2009 - <?= date('Y'); ?> PyroCMS LLC 
+			<br><span id="version"><?= CMS_VERSION.' '.CMS_EDITION; ?></span>
 		</div>
 		</div>
 	</center>
@@ -80,24 +79,24 @@
 	<script type="text/javascript">
 		Pyro = { 'lang': {} };
 
-		var APPPATH_URI					= "<?php echo APPPATH_URI;?>";
-		var SITE_URL					= "<?php echo rtrim(site_url(), '/').'/';?>";
-		var BASE_URL					= "<?php echo BASE_URL;?>";
-		var BASE_URI					= "<?php echo BASE_URI;?>";
-		var UPLOAD_PATH					= "<?php echo UPLOAD_PATH;?>";
-		var DEFAULT_TITLE				= "<?php echo addslashes(Settings::get('site_name')); ?>";
-		Pyro.current_module				= "<?php echo isset($module_details['slug']) ? $module_details['slug'] : null; ?>";
-		Pyro.admin_theme_url			= "<?php echo BASE_URL . ci()->theme->path; ?>";
-		Pyro.apppath_uri				= "<?php echo APPPATH_URI; ?>";
-		Pyro.base_uri					= "<?php echo BASE_URI; ?>";
-		Pyro.lang.remove				= "<?php echo lang('global:remove'); ?>";
-		Pyro.lang.dialog_message 		= "<?php echo lang('global:dialog:delete_message'); ?>";
-		Pyro.csrf_cookie_name			= "<?php echo config_item('cookie_prefix').config_item('csrf_cookie_name'); ?>";
+		var APPPATH_URI					= "<?= APPPATH_URI;?>";
+		var SITE_URL					= "<?= rtrim(site_url(), '/').'/';?>";
+		var BASE_URL					= "<?= BASE_URL;?>";
+		var BASE_URI					= "<?= BASE_URI;?>";
+		var UPLOAD_PATH					= "<?= UPLOAD_PATH;?>";
+		var DEFAULT_TITLE				= "<?= addslashes(Settings::get('site_name')); ?>";
+		Pyro.current_module				= "<?= isset($module_details['slug']) ? $module_details['slug'] : null; ?>";
+		Pyro.admin_theme_url			= "<?= BASE_URL . ci()->theme->path; ?>";
+		Pyro.apppath_uri				= "<?= APPPATH_URI; ?>";
+		Pyro.base_uri					= "<?= BASE_URI; ?>";
+		Pyro.lang.remove				= "<?= lang('global:remove'); ?>";
+		Pyro.lang.dialog_message 		= "<?= lang('global:dialog:delete_message'); ?>";
+		Pyro.csrf_cookie_name			= "<?= config_item('cookie_prefix').config_item('csrf_cookie_name'); ?>";
 	</script>
 
 	<?php Asset::js('build.min.js', null, 'deferred'); ?>
 
-	<?php echo Asset::render_js('deferred') ?>
+	<?= Asset::render_js('deferred') ?>
 
 </body>
 </html>
